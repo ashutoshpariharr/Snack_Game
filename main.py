f0585dc042edf164489f8a069414a84a5222afe5
Ashutosh <ashutoshparihar51@gmail.com>
2024-07-15 14:03:50 +0530

    working fine

---
+++ main.py
@@ -0,0 +1,46 @@
+import time
+from food import Food
+from snack_game import Snack
+from score_board import Scoreboard
+from turtle import Screen, Turtle
+
+screen = Screen()
+turtle = Turtle()
+snack = Snack()
+food = Food()
+score = Scoreboard()
+
+
+turtle.write("Home = ", True, align="center")
+screen.title("Welcome over Snack Game")
+screen.bgcolor("black")
+screen.tracer(0)
+screen.setup(650, 650)
+screen.listen()
+screen.onkey(snack.up, "Up")
+screen.onkey(snack.left, "Left")
+screen.onkey(snack.right, "Right")
+screen.onkey(snack.down, "Down")
+
+is_over = True
+while is_over:
+    screen.update()
+    time.sleep(0.1)
+    snack.move()
+    # Detect the food
+    if snack.head.distance(food) < 15:
+        food.refresh()
+        snack.extend()
+        score.increase_score()
+
+    # When snack hit the wall game over
+    if snack.head.xcor() > 280 or snack.head.xcor() < -280 or snack.head.ycor() > 280 or snack.head.ycor() < -280:
+        is_over = False
+        score.game_over()
+
+    for segment in snack.segment[1:]:
+        if snack.head.distance(segment) < 10:
+            is_over = False
+            score.game_over()
+
+screen.exitonclick()
